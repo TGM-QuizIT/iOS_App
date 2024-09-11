7cc19d485fa0d0817a2729ab702ba2678c252377
Marius Thaler <marius.thaler@outlook.com>
2024-09-11 15:59:47 +0200

    Initial Commit

---
+++ QuizIT/QuizITApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizITApp.swift
+//  QuizIT
+//
+//  Created by Marius on 11.09.24.
+//
+
+import SwiftUI
+
+@main
+struct QuizITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
