2212b1b8c901ff60955f9ea77eb367badde1b8ab
Raphael Tarnoczi <raphi.tarnoczi@gmail.com>
2025-01-21 10:55:57 +0100

    rewrote result struct

--- QuizIT/Model/Result.swift
+++ QuizIT/Model/Result.swift
@@ -11,6 +11,7 @@ struct Result: Codable, Identifiable, Hashable {
     var score: Double
     let userId: Int
     var focus: Focus?
+    var subject: Subject?
     let date: Date
     
     init(id: Int, score: Double, userId: Int, focus: Focus, date: Date) {
@@ -19,13 +20,25 @@ struct Result: Codable, Identifiable, Hashable {
         self.userId = userId
         self.focus = focus
         self.date = date
+        self.subject = nil
     }
+    
+    init(id: Int, score: Double, userId: Int, subject: Subject, date: Date) {
+        self.id = id
+        self.score = score
+        self.userId = userId
+        self.focus = nil
+        self.date = date
+        self.subject = subject
+    }
+    
     enum CodingKeys: String, CodingKey {
         case id = "resultId"
         case score = "resultScore"
         case date = "resultDateTime"
         case userId = "userId"
         case focus
+        case subject
     }
     
 }
